5ba61918cad1cd0cd04a1eab2ec98d0c353abd5e
Li Wan <lwan3@student.unimelb.edu.au>
2023-01-10 13:30:04 +1100

    revise

--- examples/example.py
+++ examples/example.py
@@ -4,15 +4,15 @@ import numpy as np
 from PIL import Image
 import requests
 
+
+# Step 1: Load the clip model
 model, _, preprocess = open_clip.create_model_and_transforms(model_name="ViT-L-14", pretrained="laion400m_e32",
 )
 tokenizer = open_clip.get_tokenizer("ViT-L-14")
 
 onnx_model = clip_onnx(model, source = "open_clip")
 
-
-
-
+# Step 2: conversion
 dummy = np.random.rand(1000, 800, 3) * 255
 dummy = dummy.astype("uint8")
 dummy_image = preprocess(Image.fromarray(dummy).convert("RGB")).unsqueeze(0).cpu()
@@ -24,6 +24,7 @@ onnx_model.convert_onnx32(visual_input=dummy_image, textual_input=dummy_text, ve
 onnx_model.convert_onnx16()
 
 
+# Step 3: check difference
 image = Image.open(requests.get("https://raw.githubusercontent.com/marqo-ai/marqo-clip-onnx/main/examples/coco.jpg", stream = True).raw)
 text = "a horse carrying a large load of hay and two people sitting on it"
 
